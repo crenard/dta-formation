3f322e0e29322cf96182628adf879fddc59aef6b
Cyrille <renard.cyrille@gmail.com>
2017-02-22 14:19:33 +0100

    amélioration du code

--- javaObjet/menuObjet/src/fr/menuObjet/ListObjet.java
+++ javaObjet/menuObjet/src/fr/menuObjet/ListObjet.java
@@ -7,23 +7,31 @@ public class ListObjet {
         MenuItem menuListe = new MenuListe();
         MenuItem menuAdd = new MenuAdd();
         MenuItem menuRemove = new MenuRemove();
-
+        int choix;
+        boolean choixValide = false;
+        Scanner sc = new Scanner(System.in);
         List<MenuItem> menuItemList = new ArrayList<MenuItem>();
+        
         menuItemList.add(menuListe);
         menuItemList.add(menuAdd);
         menuItemList.add(menuRemove);
         
-        System.out.println("Veuillez faire un choix");
         
-        for (MenuItem item : menuItemList) 
-        	item.printItem();
-        
-        Scanner sc = new Scanner(System.in);
-        int choix = sc.nextInt(); sc.nextLine();
-        
-        for (MenuItem item : menuItemList) {
-        	if (item.compareTo(choix))
-        		item.action();
+        while (!choixValide) {
+
+            System.out.println("Veuillez faire un choix");
+            
+	        for (MenuItem item : menuItemList) 
+	        	item.printItem();
+	        
+	        choix = sc.nextInt(); sc.nextLine();
+	        
+	        for (MenuItem item : menuItemList) {
+	        	if (item.compareTo(choix)){
+	        		item.action();
+	        		choixValide = true;
+	        	}
+	        }
         }
         sc.close();
     }
