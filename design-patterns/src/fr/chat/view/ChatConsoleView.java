3e405fef5785c9abf092f363d1ffd98e819d5a6e
Cyrille <renard.cyrille@gmail.com>
2017-03-01 16:04:37 +0100

    petite update

--- design-patterns/src/fr/chat/view/ChatConsoleView.java
+++ design-patterns/src/fr/chat/view/ChatConsoleView.java
@@ -2,14 +2,10 @@ package fr.chat.view;
 
 import java.util.*;
 
-import fr.chat.model.ChatMessage;
-import fr.chat.model.observer.ChatObservable;
-import fr.chat.model.observer.ChatObserver;
-
 public class ChatConsoleView extends ViewComposite {
 
 	public ChatConsoleView(Scanner sc) {
-		ChatConsoleTitleView titleView = new ChatConsoleTitleView("= CHAT APP");
+		ChatConsoleTitleView titleView = new ChatConsoleTitleView("= CHAT APP =");
 		this.add(titleView);
 		ChatConsoleLoginView loginView = new ChatConsoleLoginView(sc);
 		this.add(loginView);
